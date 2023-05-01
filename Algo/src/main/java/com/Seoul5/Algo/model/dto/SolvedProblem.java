1da6f47a5b600dca0ba969719c0da523cec8bde4
chickennight <john0513@naver.com>
2023-05-01 20:10:16 +0900

    dto2

--- Algo/src/main/java/com/Seoul5/Algo/model/dto/SolvedProblem.java
+++ Algo/src/main/java/com/Seoul5/Algo/model/dto/SolvedProblem.java
@@ -6,24 +6,35 @@ public class SolvedProblem {
 	private int pNum;
 	private String userId;
 	private Date solvedDate;
+
+	public SolvedProblem(int pNum, String userId, Date solvedDate) {
+		this.pNum = pNum;
+		this.userId = userId;
+		this.solvedDate = solvedDate;
+	}
+
 	public int getpNum() {
 		return pNum;
 	}
+
 	public void setpNum(int pNum) {
 		this.pNum = pNum;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public Date getSolvedDate() {
 		return solvedDate;
 	}
+
 	public void setSolvedDate(Date solvedDate) {
 		this.solvedDate = solvedDate;
 	}
-	
-	
+
 }
