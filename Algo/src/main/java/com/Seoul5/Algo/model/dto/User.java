7ad3214f35e64d1d11a1022ba33320c8037588c8
mongsuokki <121627931+mongsuokki@users.noreply.github.com>
2023-05-16 16:05:44 +0900

    Update User.java

--- Algo/src/main/java/com/Seoul5/Algo/model/dto/User.java
+++ Algo/src/main/java/com/Seoul5/Algo/model/dto/User.java
@@ -9,17 +9,15 @@ public class User {
 	private String img;
 	private String orgImg;
 	private int difficulty;
-	// 1 브론즈까지 2 실버까지 3 골드...까지 
+	// 1 브론즈까지 2 실버까지 3 골드...까지
 	private String algo;
-	// 추천문제에 어떤 알고리즘을 넣을지 
+	// 추천문제에 어떤 알고리즘을 넣을지
 	private boolean again;
 	// 추천문제에 풀었던 문제도 나올지
 	private int solvedCnt;
-
 	// 총 푼 문제수 -> 이걸 주 단위로 갱신해서 주간 랭킹 볼 수 있게?
 	private int week;
 	// 개인이 설정한 주간 문제
-	private int week;
 
 	public User(String id, String name, String password, String bOJid, String email, String img, int solvedCnt,
 			int week) {
@@ -80,43 +78,51 @@ public class User {
 	public void setImg(String img) {
 		this.img = img;
 	}
-  
+
 	public String getOrgImg() {
 		return orgImg;
 	}
-  
+
 	public void setOrgImg(String orgImg) {
 		this.orgImg = orgImg;
 	}
-  
+
 	public int getDifficulty() {
 		return difficulty;
 	}
-  
+
 	public void setDifficulty(int difficulty) {
 		this.difficulty = difficulty;
 	}
-  
+
 	public String getAlgo() {
 		return algo;
 	}
-  
+
 	public void setAlgo(String algo) {
 		this.algo = algo;
 	}
-  
+
 	public boolean isAgain() {
 		return again;
 	}
-  
+
 	public void setAgain(boolean again) {
 		this.again = again;
 	}
-  
+
+	public int getSolvedCnt() {
+		return solvedCnt;
+	}
+
+	public void setSolvedCnt(int solvedCnt) {
+		this.solvedCnt = solvedCnt;
+	}
+
 	public int getWeek() {
 		return week;
 	}
-  
+
 	public void setWeek(int week) {
 		this.week = week;
 	}
